735937af21d17cf296296e41a13f8b567fccabb8
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2018-11-09 16:56:11 -0500

    Better names

--- src/Data/List/Shuffle.hs
+++ src/Data/List/Shuffle.hs
@@ -21,7 +21,7 @@ module Data.List.Shuffle (
   -- ** List manipulation
   halve,
   -- ** Shuffling
-  riffle, bridge
+  interleave, shuffleLength
 ) where
 
 import Control.Monad.Random
@@ -48,13 +48,13 @@ halve (z:zs) = let (xs,ys) = halve zs
 -- lists, uniformly at random.  An interleaving of two lists contains all the
 -- elements from both lists, where each list’s elements are kept in order.
 --
--- >>> riffle [1,2,3,4,5] 5 [60,70,80,90] 4
+-- >>> interleave [1,2,3,4,5] 5 [60,70,80,90] 4
 -- [1,60,70,80,2,3,90,4,5]
--- >>> riffle [1,2,3,4,5] 5 [60,70,80,90] 4
+-- >>> interleave [1,2,3,4,5] 5 [60,70,80,90] 4
 -- [60,70,80,1,2,3,4,90,5]
--- >>> riffle [1,2,3,4,5] 5 [60,70,80,90] 4
+-- >>> interleave [1,2,3,4,5] 5 [60,70,80,90] 4
 -- [1,60,70,80,2,3,4,90,5]
--- >>> riffle [1,2,3,4,5] 5 [60,70,80,90] 4
+-- >>> interleave [1,2,3,4,5] 5 [60,70,80,90] 4
 -- [60,1,2,70,3,80,4,5,90]
 --
 -- If the lengths are wrong, the function will sample from the wrong
@@ -62,14 +62,14 @@ halve (z:zs) = let (xs,ys) = halve zs
 --
 -- This function is part of
 -- <https://apfelmus.nfshost.com/articles/random-permutations.html Heinrich Apfelmus’s “merge shuffle” algorithm>.
-riffle :: MonadRandom m => [a] -> Int -> [a] -> Int -> m [a]
-riffle xs     !_  []     !_  = pure xs
-riffle []     !_  ys     !_  = pure ys
-riffle (x:xs) !nx (y:ys) !ny = do
+interleave :: MonadRandom m => [a] -> Int -> [a] -> Int -> m [a]
+interleave xs     !_  []     !_  = pure xs
+interleave []     !_  ys     !_  = pure ys
+interleave (x:xs) !nx (y:ys) !ny = do
   k <- getRandomR (1, nx+ny)
   if k <= nx
-  then (x:) <$> riffle xs     (nx-1) (y:ys) ny
-  else (y:) <$> riffle (x:xs) nx     ys     (ny-1)
+  then (x:) <$> interleave xs     (nx-1) (y:ys) ny
+  else (y:) <$> interleave (x:xs) nx     ys     (ny-1)
 
 -- |Shuffle a list and compute its length.  Produces a permutation of the input uniformly at random.
 --
@@ -77,18 +77,18 @@ riffle (x:xs) !nx (y:ys) !ny = do
 -- <https://apfelmus.nfshost.com/articles/random-permutations.html Heinrich Apfelmus’s “merge shuffle” algorithm>.
 -- This variant of the merge shuffle traverses the list once.
 --
--- >>> bridge [0..9]
+-- >>> shuffleLength [0..9]
 -- ([6,3,7,5,2,1,9,4,8,0],10)
--- >>> bridge [0..9]
+-- >>> shuffleLength [0..9]
 -- ([5,2,0,7,6,9,4,1,8,3],10)
-bridge :: MonadRandom m => [a] -> m ([a],Int)
-bridge []  = pure ([],  0)
-bridge [x] = pure ([x], 1)
-bridge xs  = do
+shuffleLength :: MonadRandom m => [a] -> m ([a],Int)
+shuffleLength []  = pure ([],  0)
+shuffleLength [x] = pure ([x], 1)
+shuffleLength xs  = do
   let (half1, half2) = halve xs
-  (shuffled1, !length1) <- bridge half1
-  (shuffled2, !length2) <- bridge half2
-  shuffled              <- riffle shuffled1 length1 shuffled2 length2
+  (shuffled1, !length1) <- shuffleLength half1
+  (shuffled2, !length2) <- shuffleLength half2
+  shuffled              <- interleave shuffled1 length1 shuffled2 length2
   pure (shuffled, length1+length2)
 
 -- |Shuffle a list.  Produces a permutation of the input uniformly at random.
@@ -104,5 +104,5 @@ bridge xs  = do
 -- >>> shuffle [0..9]
 -- [8,1,2,7,9,4,5,3,6,0]
 shuffle :: MonadRandom m => [a] -> m [a]
-shuffle = fmap fst . bridge
+shuffle = fmap fst . shuffleLength
 {-# INLINE shuffle #-}
